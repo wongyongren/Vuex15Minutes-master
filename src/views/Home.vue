15c037b43bedbe57810e63f6ca0c171b6599a8fb
wongyongren <wongyongren@hotmail.com>
2021-02-23 08:31:38 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home shadow-xl bg-gray-200 w-8/12 mx-auto p-20">
     <h2 class="text-3xl text-blue-600 font-extrabold">Can I Haz Dad Jokes</h2>
-    <h3 class="h-40 text-black">{{joke}}</h3>
+    <h3 class="h-40 text-black" v-if="!(joke === 1) ">Stop</h3>
+    <h3 class="h-40 text-black" v-else>Run</h3>
     <button
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10 focus:outline-none"
       @click="addJoke"
